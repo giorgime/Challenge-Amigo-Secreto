dc683d3e8ae0fb188e45b309b4ae49c65bd334fc
Jorge Miguel <giorgime@hotmail.com>
2025-08-14 21:03:14 -0300

    crea el repositorio

---
+++ app.js
@@ -0,0 +1,24 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+/*function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+    */
+
+//Capturar el valor del campo de entrada//
+function agregarAmigo(){
+    let amigos = document.getElementById('amigo');
+    console.log(amigo);
+}
+//Validar entrada: Alert con un mensaje de error: "Por favor, inserte un nombre."//
+alert("Por favor, inserte un nombre.");
+
+
+/*Actualizar el array de amigos: Si el valor es válido, 
+añadirlo al arreglo que almacena los nombre de amigos usando el método.push(). */
+
+//Limpiar el campo de entrada: Después de añadir el nombre//
+
